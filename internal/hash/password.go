ed40dba534c4cc41aef0ac442466e935d92877ba
Andre Febrianto <andrefebrianto88@gmail.com>
2024-01-25 18:55:38 +0700

    Wrap password hash function

---
+++ internal/hash/password.go
@@ -0,0 +1,26 @@
+package hash
+
+import (
+	"devoratio.dev/web-resume/internal/errorx"
+	"golang.org/x/crypto/bcrypt"
+)
+
+const hashCost = 12
+
+func GenerateFromPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), hashCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hashedPassword), nil
+}
+
+func VerifyPassword(hashedPassword, password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	if err != nil {
+		return errorx.New(errorx.TypeUnauthorized, errorx.TypeUnauthorized.String(), err)
+	}
+
+	return nil
+}
